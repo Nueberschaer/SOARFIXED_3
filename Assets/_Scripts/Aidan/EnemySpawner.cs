c24c40eeac7d2a6315cbff8cfa92093cd6f95c67
aidank4 <aidankschool22@gmail.com>
2025-10-13 22:39:06 -0700

    Made an enemy that moves forward

--- Assets/_Scripts/Aidan/EnemySpawner.cs
+++ Assets/_Scripts/Aidan/EnemySpawner.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject fused; // spawning basic enemy later add more enemy types
-    private int spawnCount = 100; 
+    //public GameObject[] enemyTypes; //array that holds the enemy prefabs for spawning  I CANT FIGURE OUT HOW TO DO ARRAYS
 
+    public GameObject fused;
+    public GameObject advancer;
+
+    private GameObject randomEnemy;
+    Vector3 randomSpawnPosition;
+
+    private int spawnCount = 100;
 
     void Start()
     {
@@ -15,11 +21,31 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnEnemies()
     {
-        for (int count = 0; count < spawnCount; count++)
+        for (int count = 0; count < spawnCount; count++) // spawns as many enemys as the count length
         {
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(-50, 51), Random.Range(-50, 51), Random.Range(-50, 51)); //controls the position in which enemies spawn
-            // random enemypicker add once more enemies
-            Instantiate(fused, randomSpawnPosition, Quaternion.identity);
+            EnemyPicker(); //picks the enemy type
+            PositionPicker(); // picks the position
+
+            Instantiate(randomEnemy, randomSpawnPosition, Quaternion.identity); // spawns the enemy
         }
+    }
+
+    private void EnemyPicker()
+    {
+        int luckyNumber = Random.Range(0, 100); //selects between fused and advancer 75% chance of choosing fused.
+
+        if (luckyNumber < 75)
+        {
+            randomEnemy = fused;
+        }
+        else
+        {
+            randomEnemy = advancer;
+        }
+    }
+
+    private void PositionPicker()
+    {
+        randomSpawnPosition = new Vector3(Random.Range(-50, 51), Random.Range(-50, 51), Random.Range(-50, 51)); //controls the position in which enemies spawn
     }
 }
